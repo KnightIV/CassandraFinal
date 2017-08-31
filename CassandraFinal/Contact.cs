0b58aff7005e1c330e7a913e9459add1320b8769
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-08-31 13:24:03 -0600

    Modified to have a Guid property of ID to identify in Cassandra

--- CassandraFinal/Contact.cs
+++ CassandraFinal/Contact.cs
@@ -8,16 +8,33 @@ namespace CassandraFinal {
 
     public class Contact {
 
+        public Guid ID { get; set; }
         public string Name { get; set; }
         public List<int> PhoneNumbers { get; set; }
         public List<string> Emails { get; set; }
         public string Group { get; set; }
 
+        // Used by Cassandra driver to populate properties
+        public Contact() { }
+
         public Contact(string name, List<int> phoneNumbers, List<string> emails, string group) {
+            ID = Guid.NewGuid();
             Name = name;
             PhoneNumbers = phoneNumbers;
             Emails = emails;
             Group = group;
+        }
+
+        public override string ToString() {
+            string contact = $"ID: {ID}\nName: {Name}Group: {Group}\nPhone numbers:";
+            foreach (int number in PhoneNumbers) {
+                contact += $"\n\t{number}";
+            }
+            contact += "\nEmails:";
+            foreach (string email in Emails) {
+                contact += $"\n\t{email}";
+            }
+            return contact;
         }
     }
 }
