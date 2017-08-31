e00c7b667debd8be20a99ab4ab2f99455347e21b
SamKnow <sknowlton@student.neumont.edu>
2017-08-31 11:35:26 -0600

    Added InsertRow, still things to fix though

---
+++ CassandraFinal/InsertRow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CassandraFinal
+{
+    class InsertRow
+    {
+        public string Insert(Contact c)
+        {
+            string query = "INSERT INTO contacts (name, phones, emails, group) VALUES('" + c.Name + "', {";
+            foreach (int s in c.PhoneNumbers)
+            {
+                query += s.ToString();
+            }
+            query += "}, {";
+            foreach (string s in c.Emails)
+            {
+                query += "'" + s + "'";
+            }
+            query += "}); ";
+            return query;
+        }
+    }
+}
